3247503fadef4f09d073bae719fa181f022a6df8
AshwinSanthanam <ashwinsanthanam098@gmail.com>
2021-09-18 00:00:38 +0530

    Generate Jwt

--- BasicWebAPI/Controllers/AuthController.cs
+++ BasicWebAPI/Controllers/AuthController.cs
@@ -1,5 +1,12 @@
 ﻿using BasicWebApi.Models.Auth;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace BasicWebApi.Controllers
 {
@@ -7,17 +14,44 @@ namespace BasicWebApi.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         [HttpPost]
         public IActionResult AuthenticateUser([FromBody]AuthenticateUserRequestBody requestBody)
         {
             if(requestBody.Email == "ashwin@gmail.com" && requestBody.Password == "password-1")
             {
-                return new OkResult();
+                IEnumerable<Claim> claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Email, requestBody.Email)
+                };
+                string jwt = GenerateJwt(claims, DateTime.Now.AddDays(8), _configuration.GetSection("SecretKey").Value);
+                return new OkObjectResult(jwt);
             }
             else
             {
                 return new UnauthorizedResult();
             }
+        }
+
+        private string GenerateJwt(IEnumerable<Claim> claims, DateTime expiry, string key)
+        {
+            var keyBytes = Encoding.ASCII.GetBytes(key);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = expiry,
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(keyBytes), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
         }
     }
 }
