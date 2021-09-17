adf858c9cb5ab9bf6f7597b8138142a052e29314
AshwinSanthanam <ashwinsanthanam098@gmail.com>
2021-09-17 17:01:45 +0530

    Basic Login

---
+++ BasicWebAPI/Controllers/AuthController.cs
@@ -0,0 +1,23 @@
+﻿using BasicWebApi.Models.Auth;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicWebApi.Controllers
+{
+    [Route("auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost]
+        public string AuthenticateUser([FromBody]AuthenticateUserRequestBody requestBody)
+        {
+            if(requestBody.Email == "ashwin@gmail.com" && requestBody.Password == "password-1")
+            {
+                return "Login success";
+            }
+            else
+            {
+                return "Login failed";
+            }
+        }
+    }
+}
---
+++ BasicWebAPI/Models/Auth/AuthenticateUserRequestBody.cs
@@ -0,0 +1,8 @@
+﻿namespace BasicWebApi.Models.Auth
+{
+    public class AuthenticateUserRequestBody
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
